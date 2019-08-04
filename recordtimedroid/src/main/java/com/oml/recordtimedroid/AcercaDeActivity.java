16b35cd745556e75b1d55230db544463103e459d
Oscar65 <megia.oscar@gmail.com>
2019-08-04 09:37:47 +0200

    version 0.12gbeta.24 (12)
    Uploaded to play.google.com as beta.
    Now I will increase by 1 versionCode every time I upload a new version.

--- recordtimedroid/src/main/java/com/oml/recordtimedroid/AcercaDeActivity.java
+++ recordtimedroid/src/main/java/com/oml/recordtimedroid/AcercaDeActivity.java
@@ -9,7 +9,7 @@ public class AcercaDeActivity extends Activity {
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acerca_de);
-        String sAcercade = getResources().getString(R.string.mensaje_acerca_de,
+        String sAcercade = getResources().getString(R.string.mensaje_acerca_de_google,
                 BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
         final TextView mTextView = (TextView) findViewById(R.id.TextView01);
         mTextView.setText(sAcercade);
