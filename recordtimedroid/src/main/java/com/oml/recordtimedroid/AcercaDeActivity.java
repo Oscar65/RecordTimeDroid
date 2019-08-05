b9e584ed811676ccc512fbff1144c46f870f2802
Oscar65 <megia.oscar@gmail.com>
2019-08-05 10:06:53 +0200

    version 1.1g
    Delete build number from versionName.
    Now build is shown between parenthesis.
    Set text size to fit line in App.

--- recordtimedroid/src/main/java/com/oml/recordtimedroid/AcercaDeActivity.java
+++ recordtimedroid/src/main/java/com/oml/recordtimedroid/AcercaDeActivity.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Properties;
+
 public class AcercaDeActivity extends Activity {
 
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acerca_de);
-        String sAcercade = getResources().getString(R.string.mensaje_acerca_de,
-                BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
+        String sAcercade = getResources().getString(R.string.mensaje_acerca_de_google,
+                BuildConfig.VERSION_NAME, BuildConfig.VERSION_BUILD);
         final TextView mTextView = (TextView) findViewById(R.id.TextView01);
         mTextView.setText(sAcercade);
     }
