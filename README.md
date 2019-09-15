231bf333252427137af1aa52c037f3557ea587b3
Oscar65 <megia.oscar@gmail.com>
2019-09-15 21:22:47 +0200

    version 2.0g
    Added option to select lines.
    If none is selected shows on top Total elapsed time.
    If some is selected shows sum of elapsed times.

--- README.md
+++ README.md
@@ -2,16 +2,22 @@
 
 This is an Android app to record time and show elapsed time between previously recorded line.
 
-These are app screenshots:
+You can select lines and on top you will see the sum of elapsed times.
+
+These are some app screenshots:
 
 Main screen:
 
-![](Screenshot_2019-08-02-19-25-25.png)
+![](Screenshot_2019-09-15-20-28-57.png)
 
 Menu:
 
-![](Screenshot_2019-08-02-19-25-38.png)
+![](Screenshot_2019-09-15-20-46-51.png)
 
 Screen about:
 
-![](Screenshot_2019-08-03-08-07-39.png)
+![](Screenshot_2019-09-15-20-29-27.png)
+
+Selected lines:
+
+![](Screenshot_2019-09-15-20-29-14.png)
--- recordtimedroid/src/main/java/com/oml/recordtimedroid/ScrollingActivity.java
+++ recordtimedroid/src/main/java/com/oml/recordtimedroid/ScrollingActivity.java
@@ -1,6 +1,8 @@
 package com.oml.recordtimedroid;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,7 +14,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
@@ -22,13 +24,19 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 public class ScrollingActivity extends AppCompatActivity {
-    private TextView tView;
     private int linea = 1;
-    private StringBuffer sbText;
     private long lFechaAnterior;
     private String data;
     private String fileName = "lines";
+    private MyTextView mtvSumLapses;
+    private long lTotal;
+    private long lFechaInicial = -1;
+    LinearLayout csLL;
+    Context myContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +44,11 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        tView = (TextView) findViewById(R.id.tvLines);
-        sbText = new StringBuffer();
+        csLL = findViewById(R.id.cs_linear_layout);
+        mtvSumLapses = (MyTextView) findViewById(R.id.sumLapses);
 
         try {
+            myContext = this;
             String[] sFileList = this.fileList();
             if (Arrays.asList(sFileList).contains(fileName)) {
                 SimpleDateFormat sdfDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS", Locale.ROOT);
@@ -51,24 +60,56 @@ public class ScrollingActivity extends AppCompatActivity {
                 long lFecha;
                 lFechaAnterior = -1;
                 StringBuilder sbBuff = new StringBuilder();
+                mtvSumLapses.setlDiferencia(0);
 
                 while ((c = fin.read()) != -1) {
                     if (c == '\n') {
-                        lFecha = Long.parseLong(sbBuff.toString());
+                        // To be backward compatible with file format dd-MM-yyyy HH:mm:ss.SSS
+                        if (sbBuff.toString().contains("-")) {
+                            Date d = sdfDateFormat.parse(sbBuff.toString());
+                            if (d != null) {
+                                lFecha = d.getTime();
+                            } else {
+                                lFecha = 0;
+                            }
+                        } else {
+                            lFecha = Long.parseLong(sbBuff.toString());
+                        }
+                        if (lFechaInicial == -1) lFechaInicial = lFecha;
                         Date d = new Date(lFecha);
                         sFecha = sdfDateFormat.format(d);
-                        sbText.append(String.format(Locale.ROOT, "%03d | %s | ", linea++, sFecha));
+                        // Create MyTextView dynamically
+                        MyTextView mtv = new MyTextView(this);
+                        LinearLayout.LayoutParams llLP = new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+                        llLP.setMargins(0, 0, 0, 0);
+                        mtv.setLayoutParams(llLP);
+                        mtv.setText(String.format(Locale.ROOT, "%03d | %s |", linea++, sFecha));
+                        mtv.setSelected(false);
                         if (lFechaAnterior != -1) {
                             calculaDiasHorasMinutosSegundos(lFechaAnterior, lFecha, dhmsmTempo);
-                            sbText.append(String.format(Locale.ROOT, "%03d %s %02d:%02d:%02d.%03d",
+                            mtv.setText(String.format(Locale.ROOT, "%s %03d %s %02d:%02d:%02d.%03d",
+                                    mtv.getText(),
                                     (int) dhmsmTempo.getDias(), getResources().getString(R.string.dias),
                                     (int) dhmsmTempo.getHoras(),
                                     (int) dhmsmTempo.getMinutos(), (int) dhmsmTempo.getSegundos(),
                                     (int) dhmsmTempo.getMilisegundos()));
+                            mtv.setlDiferencia(lFechaAnterior - lFecha);
+
+                            calculaDiasHorasMinutosSegundos(lFechaInicial, lFecha, dhmsmTempo);
+                            mtvSumLapses.setText(String.format(Locale.ROOT, "%s %03d %s %02d:%02d:%02d.%03d",
+                                    getResources().getString(R.string.total),
+                                    (int) dhmsmTempo.getDias(), getResources().getString(R.string.dias),
+                                    (int) dhmsmTempo.getHoras(),
+                                    (int) dhmsmTempo.getMinutos(), (int) dhmsmTempo.getSegundos(),
+                                    (int) dhmsmTempo.getMilisegundos()));
+                            mtv.setOnClickListener(mtv_OnClickListener);
                         }
-                        sbText.append("\n");
                         lFechaAnterior = lFecha;
                         sbBuff.setLength(0);
+                        mtv.setTextSize( 12 );
+                        mtv.setTextIsSelectable(true);
+                        csLL.addView(mtv);
+                        lTotal = lFechaInicial - lFecha;
                     } else {
                         sbBuff.append( (char)c );
                     }
@@ -80,40 +121,67 @@ public class ScrollingActivity extends AppCompatActivity {
             Toast.makeText(getBaseContext(),"ERROR " + e.getMessage() +
                     " reading file",Toast.LENGTH_LONG).show();
         }
-        tView.setText(sbText);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Date dFechaHora = new Date();
-                long lActualFecha = dFechaHora.getTime();
-
+                long lFechaActual = dFechaHora.getTime();
                 SimpleDateFormat sdfDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS",
                         Locale.ROOT);
-                CharSequence csFechaHora = sdfDateFormat.format(dFechaHora);
-                sbText.append(String.format(Locale.ROOT, "%03d | ", linea));
-                sbText.append(csFechaHora);
-                sbText.append(" | ");
+
+                if (lFechaInicial == -1) lFechaInicial = lFechaActual;
+                String sFecha = sdfDateFormat.format(dFechaHora);
+                MyTextView mtv = new MyTextView(myContext);
+                LinearLayout.LayoutParams llLP = new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
+                llLP.setMargins(0, 2, 0, 0);
+                mtv.setLayoutParams(llLP);
+                mtv.setText(String.format(Locale.ROOT, "%03d | %s |", linea, sFecha));
                 if (linea > 1) {
                     DiasHorasMinutosSegundosMilisegundos dhmsmTempo =
                             new DiasHorasMinutosSegundosMilisegundos();
-                    calculaDiasHorasMinutosSegundos(lFechaAnterior, lActualFecha, dhmsmTempo);
-                    String sDiferencia = String.format(Locale.ROOT, "%03d %s %02d:%02d:%02d.%03d",
+                    calculaDiasHorasMinutosSegundos(lFechaAnterior, lFechaActual, dhmsmTempo);
+                    String sDiferencia = String.format(Locale.ROOT, " %03d %s %02d:%02d:%02d.%03d",
                             (int) dhmsmTempo.getDias(), getResources().getString(R.string.dias),
                             (int) dhmsmTempo.getHoras(), (int) dhmsmTempo.getMinutos(),
                             (int) dhmsmTempo.getSegundos(), (int) dhmsmTempo.getMilisegundos());
-                    sbText.append(sDiferencia);
+                    mtv.setText(String.format(Locale.ROOT, "%s %s", mtv.getText(), sDiferencia));
+                    lTotal = lFechaInicial - lFechaActual;
+                    mtv.setlDiferencia(lFechaAnterior - lFechaActual);
+                    boolean bSubTotal = false;
+
+                    for (int i = 0; i < csLL.getChildCount(); i++) {
+                        if (csLL.getChildAt(i).isSelected()) {
+                            bSubTotal = true;
+                            break;
+                        }
+                    }
+
+                    if (!bSubTotal) {
+                        calculaDiasHorasMinutosSegundos(lTotal, 0, dhmsmTempo);
+
+                        mtvSumLapses.setText(String.format(Locale.ROOT, "%s %03d %s %02d:%02d:%02d.%03d",
+                            getResources().getString(R.string.total),
+                            (int) dhmsmTempo.getDias(), getResources().getString(R.string.dias),
+                            (int) dhmsmTempo.getHoras(),
+                            (int) dhmsmTempo.getMinutos(), (int) dhmsmTempo.getSegundos(),
+                            (int) dhmsmTempo.getMilisegundos()));
+                    }
+
+                    mtv.setOnClickListener(mtv_OnClickListener);
                     String notification = getResources().getString(R.string.notificacion) + " " +
                             linea + " +" + sDiferencia;
                     Snackbar.make(view, notification, Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
+
                 linea++;
-                sbText.append("\n");
-                tView.setText(sbText);
-                lFechaAnterior = lActualFecha;
-                data = lActualFecha + "\n";
+                data = lFechaActual + "\n";
+                lFechaAnterior = lFechaActual;
+                mtv.setTextSize( 12 );
+                mtv.setTextIsSelectable(true);
+                csLL.addView(mtv);
 
                 // Save line in file with unix timestamp in milliseconds
                 try {
@@ -186,9 +254,16 @@ public class ScrollingActivity extends AppCompatActivity {
                     boolean ret = this.deleteFile(fileName);
                     if (ret) {
                         Toast.makeText(getBaseContext(), "file " + fileName + " deleted", Toast.LENGTH_LONG).show();
-                        sbText.delete(0, sbText.length());
+                        //sbText.delete(0, sbText.length());
+                        //csLL.removeAllViewsInLayout();
+                        int csLLGCC = csLL.getChildCount();
+                        for (int i = 1; i < csLLGCC; i++) {
+                            csLL.removeView(csLL.getChildAt(1));
+                        }
                         linea = 1;
-                        tView.setText(sbText);
+                        mtvSumLapses.setText("");
+                        mtvSumLapses.setlDiferencia(0);
+                        lFechaInicial = -1;
                     } else {
                         Toast.makeText(getBaseContext(), "file " + fileName + " NOT deleted", Toast.LENGTH_LONG).show();
                     }
@@ -206,5 +281,46 @@ public class ScrollingActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //On click listener for dynamic myTextView
+    final View.OnClickListener mtv_OnClickListener = new View.OnClickListener() {
+        public void onClick(final View v) {
+            if (((MyTextView) v).getSelectionStart() != ((MyTextView) v).getSelectionEnd()) return;
+            // Issue:
+            // If you click over a selected line it doesn't deselect if you click again.
+            // You need to select another line and click again the previous line to deselect it.
+            // Executes twice this method, so I set enabled to false to avoid it.
+            ((MyTextView) v).setEnabled(false);
+            boolean selected = !v.isSelected();
+            DiasHorasMinutosSegundosMilisegundos dhmsmTempo = new DiasHorasMinutosSegundosMilisegundos();
+            String sTipo;
+
+            v.setSelected(selected);
+            v.setBackgroundColor(selected ? Color.CYAN : Color.TRANSPARENT);
+            LinearLayout csLL = findViewById(R.id.cs_linear_layout);
+
+            if (selected) {
+                mtvSumLapses.setlDiferencia(mtvSumLapses.getlDiferencia() + ((MyTextView) v).getlDiferencia());
+            } else {
+                mtvSumLapses.setlDiferencia(mtvSumLapses.getlDiferencia() - ((MyTextView) v).getlDiferencia());
+            }
+
+            if (mtvSumLapses.getlDiferencia() == 0) {
+                sTipo = getResources().getString(R.string.total);
+                calculaDiasHorasMinutosSegundos(lTotal, 0, dhmsmTempo);
+            } else {
+                sTipo = getResources().getString(R.string.selected_lines);
+                calculaDiasHorasMinutosSegundos(mtvSumLapses.getlDiferencia(), 0, dhmsmTempo);
+            }
+
+            mtvSumLapses.setText(String.format(Locale.ROOT, "%s %03d %s %02d:%02d:%02d.%03d",
+                    sTipo,
+                    (int) dhmsmTempo.getDias(), getResources().getString(R.string.dias),
+                    (int) dhmsmTempo.getHoras(),
+                    (int) dhmsmTempo.getMinutos(), (int) dhmsmTempo.getSegundos(),
+                    (int) dhmsmTempo.getMilisegundos()));
+            ((MyTextView) v).setEnabled(true);
+        }
+    };
 
 }
